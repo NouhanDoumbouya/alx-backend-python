afb9393671f6b58761954befa3758ebdb96f5538
NouhanDoumbouya <nouhandoumbouya655@gmail.com>
2025-05-21 13:21:09 +0800

    task 2 fixed

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -36,5 +36,5 @@ def batch_processing(batch_size):
                 'age': user[3]
             }
             if user_dict['age'] > 25:
-                yield user_dict
+                return user_dict
 
